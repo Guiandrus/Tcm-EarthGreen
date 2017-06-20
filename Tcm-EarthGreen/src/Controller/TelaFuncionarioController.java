30db6a240c8b39b3d411b5589587f6e921e7e4be
Guiandrus <gui_vendramini@hotmail.com>
2017-06-20 11:03:06 -0300

    Add files via upload
    Atualizada a interface de quase toda as telas

---
+++ Tcm-EarthGreen/src/Controller/TelaFuncionarioController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Leo
+ */
+public class TelaFuncionarioController implements Initializable {
+
+    
+    @FXML
+    private Button btnCadastraLixo;
+
+    @FXML
+    private Button btnCadastraFornecedor;
+
+    @FXML
+    private Button btnListaFornecedores;
+
+    @FXML
+    private Button btnListaLixo;
+    
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
